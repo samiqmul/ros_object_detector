6b293e6ba48ab1ce4966d470800ac645c847c857
rohping <rohan565singh@gmail.com>
2018-03-19 22:40:10 +0900

    publish pose and bounding box

--- src/find_object.cpp
+++ src/find_object.cpp
@@ -6,6 +6,7 @@
 #include <boost/thread/thread.hpp>
 #include <Eigen/Dense>
 
+#include <eigen_conversions/eigen_msg.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <geometry_msgs/PoseStamped.h>
 
@@ -43,19 +44,20 @@ visualization_msgs::Marker lines;
 
 ros::Publisher pose_pub;
 ros::Publisher bbox_pub;
+ros::Publisher cloud_pub;
 
-float object_size_threshold_;
+float object_size_threshold_min_;
+float object_size_threshold_max_;
 int plane_normal_threshold_;
 bool use_plane_normal_threshold_;
+float roi_min_x_, roi_max_x_;
+float roi_min_y_, roi_max_y_;
+float roi_min_z_, roi_max_z_;
 
 typedef pcl::PointXYZ PCLPoint;
 typedef pcl::PointCloud<PCLPoint> PointCloud;
 typedef PointCloud::Ptr PointCloudPtr;
 
-
-
-Eigen::Vector4f minPoint (0.00,-0.20, -0.50, 0);                  //region of interest in real world (m)
-Eigen::Vector4f maxPoint (1.50, 0.20, 0.50, 0);
 
 
 void SORfilter(const PointCloudPtr input, PointCloudPtr output) {
@@ -64,10 +66,12 @@ void SORfilter(const PointCloudPtr input, PointCloudPtr output) {
   sor.setMeanK (50);
   sor.setStddevMulThresh (1);
   sor.filter (*output);
+
+  return;
 }
 
 
-void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cloud) {
+void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr& object_cloud) {
 
   int scene_cloud_point_thresh_ = 2000;
   int object_cloud_point_thresh_ = 500;
@@ -81,7 +85,7 @@ void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cl
   seg.setOptimizeCoefficients (true);
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold (0.02);
+  seg.setDistanceThreshold (0.01);
 
   PointCloudPtr object_candidate (new PointCloud);
   while(scene_cloud->points.size() > scene_cloud_point_thresh_) {
@@ -93,7 +97,7 @@ void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cl
     pcl::ExtractIndices<PCLPoint> eifilter (true);
     eifilter.setInputCloud(scene_cloud);
     eifilter.setIndices(plane_inliers);
-
+    
     if(use_plane_normal_threshold_){
       PointCloudPtr segmented_plane(new PointCloud);
       eifilter.filter(*segmented_plane);
@@ -105,7 +109,6 @@ void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cl
       pcl::compute3DCentroid(*segmented_plane,centroid);
       pcl::computeCovarianceMatrix(*segmented_plane, centroid, cov_matrix);
       pcl::eigen33(cov_matrix, evecs, evals);
-
       Eigen::Vector3f plane_normal = evecs.col(2);
       plane_normal.normalize();
       float theta = asin(plane_normal[2]) * 180/M_PI;
@@ -122,11 +125,12 @@ void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cl
     else{
       eifilter.setNegative(true);
       eifilter.filter(*object_candidate);
+      break;
     }
   }
 
   if(object_candidate->points.size() < object_cloud_point_thresh_){
-    ROS_ERROR("Object not found in point cloud");
+    ROS_ERROR("Object cloud size is less than threshold.");
     return;
   }
 
@@ -155,12 +159,78 @@ void objectSegmentation(const PointCloudPtr input_cloud, PointCloudPtr object_cl
     pcl::compute3DCentroid(*cluster,obj_centroid);
     Eigen::Vector4f plane_norm (plane_coeffs->values[0], plane_coeffs->values[1], plane_coeffs->values[2], plane_coeffs->values[3]);
     float dist = (plane_norm.dot(obj_centroid))/plane_norm.norm();
-    if(dist < object_size_threshold_){
+    if(dist > object_size_threshold_min_ && dist < object_size_threshold_max_){
       object_cloud = cluster;
       break;
     }
   }
+
+  return;
 } 
+
+
+void bboxPublisher(PointCloudPtr object_cloud, std_msgs::Header header){
+
+  visualization_msgs::MarkerArray line_list;
+  visualization_msgs::Marker lines;
+
+  lines.ns = "bounding_box";
+  lines.header = header;
+  lines.type = visualization_msgs::Marker::LINE_LIST;
+  lines.action = visualization_msgs::Marker::ADD;
+  lines.id = 1;
+  lines.color.a = 1;
+  lines.lifetime = ros::Duration(0.01);
+  lines.points.clear();
+
+  PCLPoint minp, maxp;
+  pcl::getMinMax3D(*object_cloud, minp, maxp);
+
+  geometry_msgs::Point p1,p2,p3,p4,p5,p6,p7,p8;
+  p1.x = minp.x; p1.y = minp.y; p1.z = minp.z;
+  p2.x = maxp.x; p2.y = minp.y; p2.z = minp.z;
+  p3.x = minp.x; p3.y = minp.y; p3.z = maxp.z;
+  p4.x = maxp.x; p4.y = minp.y; p4.z = maxp.z;
+  p5.x = minp.x; p5.y = maxp.y; p5.z = minp.z;
+  p6.x = maxp.x; p6.y = maxp.y; p6.z = minp.z;
+  p7.x = minp.x; p7.y = maxp.y; p7.z = maxp.z;
+  p8.x = maxp.x; p8.y = maxp.y; p8.z = maxp.z;
+
+  lines.points.push_back(p1); lines.points.push_back(p2);
+  lines.points.push_back(p1); lines.points.push_back(p3);
+  lines.points.push_back(p2); lines.points.push_back(p4);
+  lines.points.push_back(p3); lines.points.push_back(p4);
+  lines.points.push_back(p5); lines.points.push_back(p6);
+  lines.points.push_back(p5); lines.points.push_back(p7);
+  lines.points.push_back(p6); lines.points.push_back(p8);
+  lines.points.push_back(p7); lines.points.push_back(p8);
+  lines.points.push_back(p1); lines.points.push_back(p5);
+  lines.points.push_back(p2); lines.points.push_back(p6);
+  lines.points.push_back(p3); lines.points.push_back(p7);
+  lines.points.push_back(p4); lines.points.push_back(p8);
+  line_list.markers.push_back(lines);
+
+  bbox_pub.publish(line_list);
+}
+
+
+void posePublisher(Eigen::Vector3f p, Eigen::Quaternionf q, std_msgs::Header header){
+
+  geometry_msgs::Point pos;
+  tf::pointEigenToMsg(p.cast<double>(), pos);
+
+  geometry_msgs::Quaternion quat;
+  tf::quaternionEigenToMsg(q.cast<double>(), quat);
+
+  geometry_msgs::PoseStamped pos_msg;
+  pos_msg.pose.position = pos;
+  pos_msg.pose.orientation = quat;
+  pos_msg.header = header;
+
+  pose_pub.publish(pos_msg);
+
+  return;
+}
 
 
 void msgCallback(const sensor_msgs::PointCloud2ConstPtr& pcd){
@@ -168,10 +238,12 @@ void msgCallback(const sensor_msgs::PointCloud2ConstPtr& pcd){
     pcl::ScopeTime t("imgCallback");
 
     PointCloudPtr read_cloud (new PointCloud);
-    pcl::PCLPointCloud2 pcl_cloud;
-    pcl_conversions::toPCL(*pcd,pcl_cloud);
-    pcl::fromPCLPointCloud2(pcl_cloud,*read_cloud);
+    pcl::fromROSMsg(*pcd, *read_cloud);
 
+    Eigen::Vector4f minPoint (roi_min_x_, roi_min_y_, roi_min_z_, 0);                  //region of interest in real world (m)
+    Eigen::Vector4f maxPoint (roi_max_x_, roi_max_y_, roi_max_z_, 0);
+
+    //crop box filtering
     PointCloudPtr crop_cloud (new PointCloud);
     pcl::CropBox<PCLPoint> cropFilter;
     cropFilter.setInputCloud(read_cloud);
@@ -179,21 +251,50 @@ void msgCallback(const sensor_msgs::PointCloud2ConstPtr& pcd){
     cropFilter.setMax(maxPoint);
     cropFilter.filter(*crop_cloud);
 
+    //voxel grid filtering
+    /*
+    pcl::PointCloud<pcl::PointXYZ>::Ptr vox_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::VoxelGrid<pcl::PointXYZ> fo;
+    fo.setInputCloud(crop_cloud);
+    fo.setLeafSize (0.005f, 0.005f, 0.005f);
+    fo.filter (*vox_cloud);
+    */
+
     //SOR filtering
     PointCloudPtr scene(new PointCloud);
     SORfilter(crop_cloud, scene);
 
+    //segment object point cloud
     PointCloudPtr object_cloud (new PointCloud);
     objectSegmentation(scene, object_cloud);
+    if(object_cloud->points.empty()){
+      ROS_ERROR("Object cloud is empty");
+      return;
+    }
     
-    sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(*object_cloud, output);
-    output.header = pcd->header;
-    bbox_pub.publish(output);
+    //publish object pointcloud
+    PointCloudPtr output_cloud = object_cloud;
+    output_cloud->header = read_cloud->header;
+    sensor_msgs::PointCloud2 pub_msg;
+    pcl::toROSMsg(*output_cloud,pub_msg);
+    cloud_pub.publish(pub_msg);
+   
+    Eigen::Vector4f centroid;
+    Eigen::Matrix3f evecs;
+    Eigen::Vector3f evals;
+    Eigen::Matrix3f cov_matrix;
+    pcl::compute3DCentroid(*object_cloud,centroid);
+    pcl::computeCovarianceMatrix(*object_cloud, centroid, cov_matrix);
+    pcl::eigen33(cov_matrix, evecs, evals);
 
+    Eigen::Quaternionf rot(evecs);
+    Eigen::Vector3f pos = centroid.head<3>();
+    
+    //publish pose and bounding box
+    posePublisher(pos, rot, pcd->header);
+    bboxPublisher(object_cloud, pcd->header);
+   
     return;
-
-
 }
 
 
@@ -202,14 +303,21 @@ int main (int argc, char** argv){
     ros::init(argc, argv, "find_object");
     ros::NodeHandle priv_nh("~");
 
-    priv_nh.param("object_size", object_size_threshold_, float(0.2));
+    priv_nh.param("object_size_min", object_size_threshold_min_, float(0.02));
+    priv_nh.param("object_size_max", object_size_threshold_max_, float(0.2));
     priv_nh.param("plane_normal", plane_normal_threshold_, int(0));
     priv_nh.param("use_plane_normal", use_plane_normal_threshold_, bool(false));
-
+    priv_nh.param("roi_min_x", roi_min_x_, float(-0.5));
+    priv_nh.param("roi_max_x", roi_max_x_, float(0.5));
+    priv_nh.param("roi_min_y", roi_min_y_, float(-0.5));
+    priv_nh.param("roi_max_y", roi_max_y_, float(0.5));
+    priv_nh.param("roi_min_z", roi_min_z_, float(0.0));
+    priv_nh.param("roi_max_z", roi_max_z_, float(1.5));
     ros::Subscriber sub = priv_nh.subscribe("input_cloud", 1, msgCallback);
 
-    pose_pub = priv_nh.advertise<geometry_msgs::PoseStamped>("object_pose",1);
-    bbox_pub = priv_nh.advertise<sensor_msgs::PointCloud2>("bounding_box", 1);
+    pose_pub = priv_nh.advertise<geometry_msgs::PoseStamped>("output_pose",1000);
+    bbox_pub = priv_nh.advertise<visualization_msgs::MarkerArray>("output_box", 1000);
+    cloud_pub = priv_nh.advertise<sensor_msgs::PointCloud2>("output_cloud", 1000);
 
     ros::spin();
 
