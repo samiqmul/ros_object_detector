ad964c0665579f9f2e7d047ce44a9a85021ae1a9
samiqmul <m.s.siddiqui@qmul.ac.uk>
2020-12-06 15:37:44 +0000

    nov commit

---
+++ CMakeFiles/find_object.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_object.dir/src/find_object.cpp.o"
+  "/home/mustafar/catkin_ws/devel/lib/object_detector/find_object.pdb"
+  "/home/mustafar/catkin_ws/devel/lib/object_detector/find_object"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustafar/catkin_ws/src/ros_object_detector
+# Build directory: /home/mustafar/catkin_ws/src/ros_object_detector
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
